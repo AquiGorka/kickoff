df8b2eb6b9a5161419b22c605edb158ad67c8043
Gorka <gorka@aquigorka.com>
2017-05-09 19:29:32 +0200

    Adds hello world go script

---
+++ game/src/game.go
@@ -0,0 +1,16 @@
+// hello world from go
+// edited from my local computer with my current editor
+// but run inside a container with go
+// go install game
+// or cd into directory and go install
+// run with $GOPATH/bin/game
+// or simply with game as $GOPATH/bin has been added to PATH
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Printf("Hello, world from Go.")
+}
+
