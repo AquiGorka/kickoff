fc4e7b1b5e1c79cbf6a9573d23fb4eb33bdcd667
Gorka <gorka@aquigorka.com>
2017-05-20 12:55:25 +0200

    Adds request logger and graceful shutdown handler

--- game/src/game.go
+++ game/src/game.go
@@ -1,52 +1,58 @@
-package main 
+package main
 
 import (
+  "context"
+  "time"
   "gopkg.in/kataras/iris.v6"
   "gopkg.in/kataras/iris.v6/adaptors/httprouter"
+  "gopkg.in/kataras/iris.v6/middleware/logger"
 )
 
 func hello(ctx *iris.Context) {
-    ctx.Writef("Hello from %s", ctx.Path())
-}
-
-func notFound(ctx *iris.Context) {
-  ctx.HTML(iris.StatusNotFound, "<h1>404</h1>")
+  ctx.HTML(iris.StatusOK, "Hello!! from " + ctx.Path())
 }
 
 func main() {
   app := iris.New()
+
   // output startup banner and error logs on os.Stdout
-  //app.Adapt(iris.DevLogger())
+  app.Adapt(iris.DevLogger())
+
   // set the router, you can choose gorillamux too
   app.Adapt(httprouter.New())
 
-  app.Get("/", hello)
-  app.OnError(iris.StatusNotFound, notFound)
+  // request logger
+  customLogger := logger.New(logger.Config{
+    // Status displays status code
+    Status: true,
+    // IP displays request's remote address
+    IP: true,
+    // Method displays the http method
+    Method: true,
+    // Path displays the request path
+    Path: true,
+  })
+  app.Use(customLogger)
 
-/*
+  // 404
+  errorLogger := logger.New()
   app.OnError(iris.StatusNotFound, func(ctx *iris.Context) {
-    ctx.HTML(iris.StatusNotFound, "<h1>Custom not found handler </h1>")
+    errorLogger.Serve(ctx)
+    ctx.HTML(iris.StatusNotFound, "<h1>404</h1>")
   })
-*/
-/*
-  app.HandleFunc("GET", "/", func(ctx *iris.Context) {
-    ctx.Writef("hello world\n")
-  })
-*/
-/*
-  app.Get("/hi", func(ctx *iris.Context) {
-    ctx.HTML(iris.StatusOK, " <h1>hi, I just exist in order to see if the server is closed</h1>")
-  })
-*/
-/*
+
+  // hello
+  app.Get("/", hello)
+
   app.Adapt(iris.EventPolicy{
     // Interrupt Event means when control+C pressed on terminal.
     Interrupted: func(*iris.Framework) {
       // shut down gracefully, but wait 5 seconds the maximum before closed
-      ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+      ctx, _ := context.WithTimeout(context.Background(), 5 * time.Second)
       app.Shutdown(ctx)
     },
   })
-*/
+
+  // execute the http server
   app.Listen(":8080")
 }
