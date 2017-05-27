3503200ae92b1b93590a9c972217c173a5b79fc3
Gorka <gorka@aquigorka.com>
2017-05-27 14:37:13 +0200

    Adds workdir to docker run script

--- game/src/scripts/docker.sh
+++ game/src/scripts/docker.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --volume "$PWD/src:/go/src/game" --volume "$PWD/lib:/go/src" --name kickoff-game --entrypoint /bin/bash -p 8877:8080 golang
+docker run -it --rm --volume "$PWD/src:/go/src/game" --volume "$PWD/lib:/go/src" --name kickoff-game --workdir "/go/src/game" --entrypoint /bin/bash -p 8877:8080 golang
