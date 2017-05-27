3ec968fde46ac8a295b1c28b9353d87d1dda4356
Gorka <gorka@aquigorka.com>
2017-05-27 14:50:43 +0200

    Moves server to independent package

---
+++ game/src/server/server.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+  "context"
+  "time"
+  "gopkg.in/kataras/iris.v6"
+  "gopkg.in/kataras/iris.v6/adaptors/httprouter"
+  "gopkg.in/kataras/iris.v6/middleware/logger"
+)
+
+func index(ctx *iris.Context) {
+  ctx.HTML(iris.StatusOK, "")
+}
+
+func HttpServer(httpPort string) {
+  app := iris.New()
+
+  // output startup banner and error logs on os.Stdout
+  app.Adapt(iris.DevLogger())
+
+  // set the router, you can choose gorillamux too
+  app.Adapt(httprouter.New())
+
+  // request logger
+  customLogger := logger.New(logger.Config{
+    // Status displays status code
+    Status: true,
+    // IP displays request's remote address
+    IP: true,
+    // Method displays the http method
+    Method: true,
+    // Path displays the request path
+    Path: true,
+  })
+  app.Use(customLogger)
+
+  // 404
+  errorLogger := logger.New()
+  app.OnError(iris.StatusNotFound, func(ctx *iris.Context) {
+    errorLogger.Serve(ctx)
+    ctx.HTML(iris.StatusNotFound, "")
+  })
+
+  // index
+  app.Get("/", index)
+
+  app.Adapt(iris.EventPolicy{
+    // Interrupt Event means when control+C pressed on terminal.
+    Interrupted: func(*iris.Framework) {
+      // shut down gracefully, but wait 5 seconds the maximum before closed
+      ctx, _ := context.WithTimeout(context.Background(), 5 * time.Second)
+      app.Shutdown(ctx)
+    },
+  })
+
+  // execute the http server
+  app.Listen(":" + httpPort)
+}
